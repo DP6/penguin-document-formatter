75f60caf586c07c03a06ea32d26b8201c9d4ae48
sr-chico <62573687+sr-chico@users.noreply.github.com>
2020-12-15 15:49:20 -0300

    Update formatEvents.js

--- src/formatEvents.js
+++ src/formatEvents.js
@@ -1,12 +1,11 @@
-
 exports.formatEvents = formatEvents;
 
 function formatEvents(events, info, pagina) {
     let pageview, eventos;
+    
     pageview = events.filter((item) => /pageview/i.test(item[0].key) || /page_view/.test(item[0].value))
     pageview = pageview ? pageview[0] : pageview;
     let pageview_index = events.indexOf(pageview);
-
     if (pageview != undefined && pageview_index != -1)
         events.splice(pageview_index, 1);
 
@@ -22,20 +21,24 @@ function formatEvents(events, info, pagina) {
             nome_mapa: info.page
         }
     }) : null;
-
+    
     const pagePath = pageview[0].pagename;
-
+    
     eventos = events.length > 0 ? events.map(([evento, category, action, label]) => {
-        return {
-            eventType: evento.value,
-            eventCategory: category.value,
-            eventAction: action.value,
-            eventLabel: label.value,
-            pagename: pagePath,
-            versao: info.version,
-            tela: info.screen,
-            pagina_mapa: pagina,
-            nome_mapa: info.page
+        if(evento.key == "Evento:"){
+            return {
+                eventType: evento.value,
+                eventCategory: category.value,
+                eventAction: action.value,
+                eventLabel: label.value,
+                pagename: pagePath,
+                versao: info.version,
+                tela: info.screen,
+                pagina_mapa: pagina,
+                nome_mapa: info.page
+            }
+        }else{
+            return null;
         }
     }) : null;
     return { pageview, eventos };
