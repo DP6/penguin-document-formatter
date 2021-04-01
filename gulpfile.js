03e0c3f6274a83e49e786d0da6afa5ae59687ff1
1cadumagalhaes <magalhaescarlosedu@gmail.com>
2021-03-31 23:23:20 -0300

    fix: criacao da exportacao do zip das funcoes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 const { series, parallel, src, dest, watch } = require('gulp');
 const mocha = require('gulp-mocha');
+const zip = require('gulp-zip');
 
-function moveConfig() {
-    return src('src/config.json').pipe(dest('functions/extract-events'));
-}
-
+//Convert pdf to json
 function extractJson() {
     return src('src/pdf2text.js').pipe(dest('functions/convert-pdf-to-json/'));
 }
 
+function convertHub() {
+    return src('src/hub.js').pipe(dest('functions/convert-pdf-to-json/'));
+}
+
+function convertFirestore() {
+    return src('src/firestore.js').pipe(dest('functions/convert-pdf-to-json/'));
+}
+
+function zipConvert() {
+    return src('functions/convert-pdf-to-json')
+        .pipe(zip('convert-pdf-to-json.zip'))
+        .pipe(dest('dist'))
+}
+
+
+//Extract events from file
 function saveFile() {
     return src('src/saveFile.js').pipe(dest('functions/extract-events'));
 }
@@ -25,8 +39,30 @@ function bigQuery() {
     return src('src/bigquery.js').pipe(dest('functions/extract-events'));
 }
 
+function extractHub() {
+    return src('src/hub.js').pipe(dest('functions/extract-events/'));
+}
+
+function extractFirestore() {
+    return src('src/firestore.js').pipe(dest('functions/extract-events/'));
+}
+
+
+function zipExtract() {
+    return src('functions/extract-events')
+        .pipe(zip('extract-events.zip'))
+        .pipe(dest('dist'))
+}
+
 function test() {
     return src(['test/*.js']).pipe(mocha());
 }
 
-exports.build = series(test, parallel(moveConfig, extractJson, saveFile, extractEvents, formatEvents));
+
+
+exports.build = series(
+    test,
+    parallel(extractJson, convertHub, convertFirestore),
+    parallel(saveFile, extractEvents, formatEvents, extractHub, extractFirestore),
+    parallel(zipConvert, zipExtract)
+);
